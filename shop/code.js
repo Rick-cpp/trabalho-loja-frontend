6363f1f93f7a5c7f6b3e92ede89749951abfa1ff
Rickz <91611484+Rick-cpp@users.noreply.github.com>
2025-08-29 12:49:46 -0300

    inicio do JS

---
+++ shop/code.js
@@ -0,0 +1,9 @@
+function createItem() {
+    let list = [];
+    data.produtos[0].
+    data.produtos.forEach(produto => {
+        let (nome, desc, shop, link, imag) = produto;
+        
+    });
+}
+
---
+++ shop/data.js
@@ -0,0 +1,14 @@
+const data = {
+    "produtos": [
+        {
+            "nome": "",
+            "desc": "",
+            "shop": 0.0,
+            "link": "",
+            "imag": [
+                "imag1",
+                "imag2"
+            ]
+        }
+    ]
+}
